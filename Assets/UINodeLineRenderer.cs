4c5788e4915edb904291e34f079a14c3dc1bf942
pluszak <paul.luszuk@wearedelivr.com>
2020-06-09 17:27:16 +0200

    [add] star project + first node + line renderer ui

---
+++ Assets/UINodeLineRenderer.cs
@@ -0,0 +1,34 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.UI.Extensions;
+
+public class UINodeLineRenderer : UILineRenderer
+{
+    private bool updateLine;
+    private readonly int x = Screen.width / 2;
+    private readonly int y = Screen.height / 2;
+    
+    [UsedImplicitly]
+    public void StartLine()
+    {
+        updateLine = true;
+        var position = transform.position;
+        m_points[0] = new Vector2(position.x, position.y);
+    }
+    
+    [UsedImplicitly]
+    public void EndLine()
+    {
+        updateLine = false;
+    }
+
+    // talk dirty to update line renderer and set mose pos
+    // todo: this is for demo only, to heavy for production code 
+    private void Update()
+    {
+        if (!updateLine) return;
+        SetAllDirty();
+        var position = Input.mousePosition;
+        m_points[1] = new Vector2(position.x - x, position.y - y);
+    }
+}
---
+++ Assets/unity-ui-extensions/Examples/UILineRenderer/TestAddingPoints.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace UnityEngine.UI.Extensions.Examples
+{
+    public class TestAddingPoints : MonoBehaviour
+    {
+        public UILineRenderer LineRenderer;
+        public Text XValue;
+        public Text YValue;
+
+        // Use this for initialization
+
+
+        public void ClearPoints()
+        {
+            LineRenderer.Points = new Vector2[0];
+        }
+    }
+}
